95253538c46c144c032c22b4d7d22083f4595363
komalahire <komala@navgurukul.org>
2021-06-22 16:38:07 +0530

    Page and limit  integration with  backend

--- src/components/Dashboard /PartnerData/index.js
+++ src/components/Dashboard /PartnerData/index.js
@@ -8,59 +8,48 @@ import { PATHS } from "../../../constant";
 import { useSelector } from "react-redux";
 
 function PartnerDashboard() {
-  const [pageNumber, setPageNumber] = useState(); //current page
+  const [pageNumber, setPageNumber] = useState(0); //current page
+
   const [partners, setPartners] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [ascendingAlphabetically, setAscendingAlphabetically] = useState(true);
-  const [ascendingNumerically, setAscendingNumerically] = useState(true);
-  const [loading, setLoading] = useState(true);
   const [debouncedText] = useDebounce(searchTerm);
   const user = useSelector(({ User }) => User);
 
   useEffect(() => {
     axios
-      .get(` https://api.merakilearn.org/partners`, {
-        headers: { Authorization: user.data.token },
-      })
+      .get(
+        `http://dev-api.navgurukul.org/apiDocs/partners?limit=${10}&page=${
+          pageNumber + 1
+        }`,
+        {
+          headers: { Authorization: user.data.token },
+        }
+      )
       .then((res) => {
-        // console.log(res, "data");
+        // console.log(res, "give me 10 data");
         setPartners(res.data);
       });
-  }, []);
+  }, [pageNumber]);
 
-  const usersPerPage = 10;
-  const pagesVisited = pageNumber * usersPerPage;
-
-  // const pageCount = Math.ceil(partners.length / usersPerPage);
-
-  const pageCount = 5;
-  // const pageCount = 24;
-
-  const number = 0;
+  // const number = 0;
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
 
-  const Partners = partners.filter((searchValue) => {
-    if (searchTerm == "") {
-      return searchValue;
-    } else if (
-      searchValue.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ) {
-      return searchValue;
-    }
-  });
+  // const pagecount = (page, limit = 10) => {
+  //   console.log(page,'page')
 
-  const sortByName = () => {
-    setAscendingAlphabetically(!ascendingAlphabetically);
-    setLoading(true);
-  };
+  //   axios
+  //     .get(`http://dev-api.navgurukul.org/apiDocs/partners?limit=${limit}&page=${page}`, {
+  //       headers: { Authorization: user.data.token },
+  //     })
+  //     .then((res) => {
+  //       console.log(res, 'llllll=====')
+  //       setPartners(res.data);
+  //     });
 
-  const sortByStudentsNumber = () => {
-    setAscendingNumerically(!ascendingNumerically);
-    setLoading(false);
-  };
+  // }
 
   return (
     <>
@@ -81,120 +70,65 @@ function PartnerDashboard() {
             <ReactPaginate
               previousLabel={"Previous"}
               nextLabel={"Next"}
-              pageLabelBuilder={(page) => page + 5 * number}
-              // pageRangeDisplayed={5}
-              pageCount={pageCount}
               initialPage={0}
-              // marginPagesDisplayed={0}
+              marginPagesDisplayed={0}
               onPageChange={changePage}
               containerClassName={"paginationBttns"}
               previousLinkClassName={"previousBttn"}
               nextLinkClassName={"nextBttn"}
               disabledClassName={"paginationDisabled"}
               activeClassName={"paginationActive"}
+              // pageLabelBuilder={(page) => page + 5 * number}
+              // pageRangeDisplayed={5}
+              // pageCount={5}
+
+              // onClick={(num)=>{
+              //   console.log(num,'num')
+              // }}
+              // eventListener={'onClick'}
+              // onPageActive={(number) => {
+              //   console.log(number, 'number--')
+              //   // pagecount( number.selected+1)
+              // }}
             />
           </div>
         </div>
         <table className="table">
           <thead>
             <tr>
-              <th>
-                Partners Name
-                <button type="button" onClick={sortByName}>
-                  A-Z/Z-A
-                </button>
-              </th>
-              <th>
-                Number of students
-                <button type="button" onClick={sortByStudentsNumber}>
-                  ^
-                </button>
-              </th>
+              <th>Partners Name</th>
+              <th>Number of students</th>
             </tr>
           </thead>
           <tbody>
-            {loading
-              ? ascendingAlphabetically
-                ? Partners.slice(pagesVisited, pagesVisited + usersPerPage).map(
-                    (item) => {
-                      return (
-                        <tr key={item.id}>
-                          <td data-label="Name">
-                            <Link
-                              className="t-data"
-                              to={`${PATHS.PARTNERS}/${item.id}`}
-                            >
-                              {" "}
-                              {item.name}
-                            </Link>
-                          </td>
-                          <td data-label="Total students">{item.users}</td>
-                        </tr>
-                      );
-                    }
-                  )
-                : Partners.reverse()
-                    .slice(pagesVisited, pagesVisited + usersPerPage)
-                    .map((item) => {
-                      return (
-                        <tr key={item.id}>
-                          <td data-label="Name">
-                            <Link
-                              className="t-data"
-                              to={`${PATHS.PARTNERS}/${item.id}`}
-                            >
-                              {" "}
-                              {item.name}
-                            </Link>
-                          </td>
-                          <td data-label="Total students">{item.users}</td>
-                        </tr>
-                      );
-                    })
-              : ascendingNumerically
-              ? Partners.slice(0)
-                  .sort(function (a, b) {
-                    return a.users - b.users;
-                  })
-                  .slice(pagesVisited, pagesVisited + usersPerPage)
-                  .map((item) => {
-                    return (
-                      <tr key={item.id}>
-                        <td data-label="Name">
-                          <Link
-                            className="t-data"
-                            to={`${PATHS.PARTNERS}/${item.id}`}
-                          >
-                            {" "}
-                            {item.name}
-                          </Link>
-                        </td>
-                        <td data-label="Total students">{item.users}</td>
-                      </tr>
-                    );
-                  })
-              : Partners.slice(0)
-                  .sort(function (a, b) {
-                    return a.users - b.users;
-                  })
-                  .reverse()
-                  .slice(pagesVisited, pagesVisited + usersPerPage)
-                  .map((item) => {
-                    return (
-                      <tr key={item.id}>
-                        <td data-label="Name">
-                          <Link
-                            className="t-data"
-                            to={`${PATHS.PARTNERS}/${item.id}`}
-                          >
-                            {" "}
-                            {item.name}
-                          </Link>
-                        </td>
-                        <td data-label="Total students">{item.users}</td>
-                      </tr>
-                    );
-                  })}
+            {partners
+              .filter((searchValue) => {
+                if (searchTerm == "") {
+                  return searchValue;
+                } else if (
+                  searchValue.name
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                ) {
+                  return searchValue;
+                }
+              })
+              .map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td data-label="Name">
+                      <Link
+                        className="t-data"
+                        to={`${PATHS.PARTNERS}/${item.id}`}
+                      >
+                        {" "}
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td data-label="Total students">{item.users}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
