1a8246c33cb482d5f5845bf2890eb7a27ff5bbd0
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2021-08-27 16:39:55 +0530

    Unable to sort column last class date

--- src/components/Dashboard /PartnerData/index.js
+++ src/components/Dashboard /PartnerData/index.js
@@ -9,6 +9,7 @@ import { useDebounce } from "use-debounce";
 import { PATHS } from "../../../constant";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import { BsArrowUpDown } from "react-icons/bs";
 
 toast.configure();
 
@@ -17,6 +18,10 @@ function PartnerDashboard() {
   const [totalCount, setTotalCount] = useState();
   const [partners, setPartners] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [ascendingPartnersNames, setAscendingPartnersNames] = useState(true);
+  const [ascendingPartnerStudentsNumber, setAscendingPartnerStudentsNumber] =
+    useState(true);
+  const [ascendingAlphabetically, setAsscendingAlphabetically] = useState(true);
   const [debouncedText] = useDebounce(searchTerm, 400);
   const user = useSelector(({ User }) => User);
   const limit = 10;
@@ -42,6 +47,94 @@ function PartnerDashboard() {
   const pageCount = Math.ceil(totalCount / limit);
   const changePage = ({ selected }) => {
     setPageNumber(selected);
+  };
+
+  const partnersData = (item) => {
+    return (
+      <tr key={item.id}>
+        <td data-column="Name">
+          <Link className="t-data" to={`${PATHS.PARTNERS}/${item.id}`}>
+            {" "}
+            {item.name}
+          </Link>
+        </td>
+        <td data-column="Total students">{item.users}</td>
+        {item.meraki_link ? (
+          <td data-column="Meraki Link">
+            <a
+              className="meraki_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.meraki_link}
+            >
+              Get Link
+            </a>
+            <CopyToClipboard
+              text={item.meraki_link}
+              onCopy={() => {
+                toast.success("Copied to Clipboard", {
+                  position: toast.POSITION.BOTTOM_RIGHT,
+                  autoClose: 1200,
+                });
+              }}
+            >
+              <i className="clipboard fa fa-copy"></i>
+            </CopyToClipboard>
+          </td>
+        ) : (
+          <td data-column="Meraki Link">
+            <div
+              className="create-link"
+              onClick={() => createMerakiLink(item.id, "android")}
+            >
+              Create link
+            </div>
+          </td>
+        )}
+        {item.web_link ? (
+          <td data-column="Meraki Link">
+            <a
+              className="meraki_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.web_link}
+            >
+              Get Link
+            </a>
+            <CopyToClipboard
+              text={item.web_link}
+              onCopy={() => {
+                toast.success("Copied to Clipboard", {
+                  position: toast.POSITION.BOTTOM_RIGHT,
+                  autoClose: 1200,
+                });
+              }}
+            >
+              <i className="clipboard fa fa-copy"></i>
+            </CopyToClipboard>
+          </td>
+        ) : (
+          <td data-column="Meraki Link">
+            <div
+              className="create-link"
+              onClick={() => createMerakiLink(item.id, "web")}
+            >
+              Create link
+            </div>
+          </td>
+        )}
+      </tr>
+    );
+  };
+
+  const sortByName = () => {
+    setAscendingPartnersNames(!ascendingPartnersNames);
+    setAsscendingAlphabetically(true);
+  };
+
+  const sortByNumber = () => {
+    setAscendingPartnerStudentsNumber(!ascendingPartnerStudentsNumber);
+    setAsscendingAlphabetically(false);
   };
 
   const createMerakiLink = (id, platform) => {
@@ -102,93 +195,67 @@ function PartnerDashboard() {
         <table className="partners-table">
           <thead>
             <tr>
-              <th>Partner's Name</th>
-              <th>Number of students</th>
+              <th>
+                Partner's Name
+                <button
+                  type="button"
+                  onClick={sortByName}
+                  className="sortNameButton"
+                >
+                  <BsArrowUpDown />
+                </button>
+              </th>
+              <th>
+                Number of students
+                <button
+                  type="button"
+                  onClick={sortByNumber}
+                  className="sortNumberButton"
+                >
+                  <BsArrowUpDown />
+                </button>
+              </th>
               <th>Meraki - Android Link</th>
               <th>Meraki - Web Link</th>
             </tr>
           </thead>
           <tbody>
-            {partners.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td data-column="Name">
-                    <Link
-                      className="t-data"
-                      to={`${PATHS.PARTNERS}/${item.id}`}
-                    >
-                      {" "}
-                      {item.name}
-                    </Link>
-                  </td>
-                  <td data-column="Total students">{item.users}</td>
-                  {item.meraki_link ? (
-                    <td data-column="Meraki Link">
-                      <a
-                        className="meraki_link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={item.meraki_link}
-                      >
-                        Get Link
-                      </a>
-                      <CopyToClipboard
-                        text={item.meraki_link}
-                        onCopy={() => {
-                          toast.success("Copied to Clipboard", {
-                            position: toast.POSITION.BOTTOM_RIGHT,
-                            autoClose: 1200,
-                          });
-                        }}
-                      >
-                        <i className="clipboard fa fa-copy"></i>
-                      </CopyToClipboard>
-                    </td>
-                  ) : (
-                    <td data-column="Meraki Link">
-                      <div
-                        className="create-link"
-                        onClick={() => createMerakiLink(item.id, "android")}
-                      >
-                        Create link
-                      </div>
-                    </td>
-                  )}
-                  {item.web_link ? (
-                    <td data-column="Meraki Link">
-                      <a
-                        className="meraki_link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={item.web_link}
-                      >
-                        Get Link
-                      </a>
-                      <CopyToClipboard
-                        text={item.web_link}
-                        onCopy={() => {
-                          toast.success("Copied to Clipboard", {
-                            position: toast.POSITION.BOTTOM_RIGHT,
-                            autoClose: 1200,
-                          });
-                        }}
-                      >
-                        <i className="clipboard fa fa-copy"></i>
-                      </CopyToClipboard>
-                    </td>
-                  ) : (
-                    <td data-column="Meraki Link">
-                      <div
-                        className="create-link"
-                        onClick={() => createMerakiLink(item.id, "web")}
-                      >
-                        Create link
-                      </div>
-                    </td>
-                  )}
-                </tr>
-              );
-            })}
+            {ascendingAlphabetically
+              ? ascendingPartnersNames
+                ? partners
+                    .sort(function (a, b) {
+                      const nameA = a.name.toLowerCase();
+                      const nameB = b.name.toLowerCase();
+                      return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                    })
+                    .map((item) => {
+                      return partnersData(item);
+                    })
+                : partners
+                    .sort(function (a, b) {
+                      const nameA = a.name.toLowerCase();
+                      const nameB = b.name.toLowerCase();
+                      return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                    })
+                    .reverse()
+                    .map((item) => {
+                      return partnersData(item);
+                    })
+              : ascendingPartnerStudentsNumber
+              ? partners
+                  .sort(function (a, b) {
+                    return b.users - a.users;
+                  })
+                  .map((item) => {
+                    return partnersData(item);
+                  })
+              : partners
+                  .sort(function (a, b) {
+                    return a.users - b.users;
+                  })
+                  .map((item) => {
+                    return partnersData(item);
+                  })}
           </tbody>
         </table>
       </div>
--- src/components/Dashboard /StudentData/index.js
+++ src/components/Dashboard /StudentData/index.js
@@ -5,6 +5,7 @@ import { METHODS } from "../../../services/api";
 import { useDebounce } from "use-debounce";
 import ReactPaginate from "react-paginate";
 import moment from "moment";
+import { BsArrowUpDown } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "./styles.scss";
 
@@ -22,6 +23,18 @@ function StudentData() {
   const [message, setMessage] = useState("");
   const [students, setStudents] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [ascendingByStudentName, setAscendingByStudentName] = useState(true);
+  const [ascendingByDate, setAscendingByDate] = useState(true);
+  const [ascendingByEnrollDate, setAscendingByEnrollDate] = useState(true);
+  const [ascendingByLastDate, setAscendingByLastDate] = useState(true);
+  const [ascendingByTotalClass, setAscendingByTotalClass] = useState(true);
+  const [ascendingByLastClassTitle, setAscendingByLastClassTitle] =
+    useState(true);
+  const [ascendingByName, setAscendingByName] = useState(true);
+  const [ascendingByRating, setAscendingByRating] = useState(true);
+  const [ascendingAlphabetically, setAscendingAlphabetically] = useState(true);
+  const [ascendingNumerically, setAscendingNumerically] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [debouncedText] = useDebounce(searchTerm, 400);
   const user = useSelector(({ User }) => User);
 
@@ -72,6 +85,133 @@ function StudentData() {
     setPageNumber(selected);
   };
 
+  const studentList = students.map((item) => {
+    let lastClassTitle =
+      item.classes_registered.length > 0 &&
+      item.classes_registered[item.classes_registered.length - 1]["title"] != ""
+        ? item.classes_registered[item.classes_registered.length - 1]["title"]
+        : "NA";
+    item.lastClassTitle = lastClassTitle;
+    let lastDate =
+      item.classes_registered.length > 0 &&
+      item.classes_registered[item.classes_registered.length - 1]["start_time"]
+        ? item.classes_registered[item.classes_registered.length - 1][
+            "start_time"
+          ]
+        : "NA";
+    item.lastDate = lastDate;
+    let LastClassTime =
+      item.classes_registered.length > 0 &&
+      item.classes_registered[item.classes_registered.length - 1]["end_time"]
+        ? item.classes_registered[item.classes_registered.length - 1][
+            "end_time"
+          ]
+        : "NA";
+    item.LastClassTime = LastClassTime;
+    let getStars = 0;
+    let totalStarts = item.classes_registered.length * 5;
+    item.classes_registered.map((stars) => {
+      getStars = getStars + Number(stars.feedback.feedback);
+    });
+    item.avgClassRating = Math.ceil(getStars / totalStarts);
+    if (isNaN(item.avgClassRating)) {
+      item.avgClassRating = 0;
+    }
+    return item;
+  });
+
+  const studentData = (item) => {
+    return (
+      <tr key={item.id}>
+        <td data-column="Name">
+          <Link
+            className="t-data"
+            to={{
+              pathname: "/student",
+              state: {
+                pass: item.classes_registered,
+                passName: item.name,
+              },
+            }}
+          >
+            {item.name}
+          </Link>
+        </td>
+        <td data-column="Enrolled On">{item.created_at}</td>
+        <td data-column="Total classes "> {item.classes_registered.length}</td>
+        <td data-column="Last class title">{item.lastClassTitle}</td>
+        <td data-column="Last class date">{item.lastDate}</td>
+        <td data-column="Last class time">{item.LastClassTime}</td>
+        <td data-column="Avg Class Rating ">
+          {[1, 2, 3, 4, 5].map((star) => {
+            return item.avgClassRating > 0 && star <= item.avgClassRating ? (
+              <span className="fa fa-star" style={{ color: "#D55F31" }}></span>
+            ) : (
+              <span className="fa fa-star" style={{ color: "gray" }}></span>
+            );
+          })}
+        </td>
+      </tr>
+    );
+  };
+
+  const sortByStudentsName = () => {
+    setAscendingByStudentName(!ascendingByStudentName);
+    setAscendingByName(true);
+    setAscendingAlphabetically(true);
+    setLoading(true);
+  };
+
+  const sortByLastClassTitle = () => {
+    setAscendingByLastClassTitle(!ascendingByLastClassTitle);
+    setAscendingByName(false);
+    setAscendingAlphabetically(true);
+    setLoading(true);
+  };
+
+  const sortByEnrollDate = () => {
+    setAscendingByEnrollDate(!ascendingByEnrollDate);
+    setAscendingByDate(true);
+    setAscendingAlphabetically(false);
+    setLoading(true);
+  };
+
+  const sortByLastClassDate = () => {
+    setAscendingByLastDate(!ascendingByLastDate);
+    setAscendingByDate(false);
+    setAscendingAlphabetically(false);
+    setLoading(true);
+  };
+
+  const sortByTotalClass = () => {
+    setAscendingByTotalClass(!ascendingByTotalClass);
+    setAscendingNumerically(true);
+    setLoading(false);
+  };
+
+  const sortByRating = () => {
+    setAscendingByRating(!ascendingByRating);
+    setAscendingNumerically(false);
+    setLoading(false);
+  };
+
+  const lastDate = studentList.filter((i) => i.lastDate !== "NA");
+  const NA = studentList.filter((i) => i.lastDate === "NA");
+  lastDate.sort(function (a, b) {
+    const dateA = new Date(a.lastDate);
+    const dateB = new Date(b.lastDate);
+    console.log("lastDate", typeof a.lastDate);
+    return dateB - dateA;
+  });
+  const studentClassLastDate = [...lastDate, ...NA];
+  studentList.sort(function (a, b) {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    console.log("studentList", typeof a.created_at);
+    return dateB - dateA;
+  });
+  console.log("lastDate", lastDate);
+
   return (
     <div className="container-table">
       <div className="container-for-search">
@@ -105,96 +245,175 @@ function StudentData() {
       <table className="student-overview-table">
         <thead>
           <tr>
-            <th>Students Name</th>
-            <th>Enroll date </th>
-            <th>Total Classes Attended</th>
-            <th>Last Class Title</th>
-            <th>Last Class Date </th>
+            <th>
+              Students Name
+              <button
+                type="button"
+                onClick={sortByStudentsName}
+                className="sort"
+              >
+                <BsArrowUpDown />
+              </button>
+            </th>
+            <th>
+              Enroll date
+              <button type="button" onClick={sortByEnrollDate} className="sort">
+                <BsArrowUpDown />
+              </button>
+            </th>
+            <th>
+              Total Classes Attended
+              <button type="button" onClick={sortByTotalClass} className="sort">
+                <BsArrowUpDown />
+              </button>
+            </th>
+            <th>
+              Last Class Title
+              <button
+                type="button"
+                onClick={sortByLastClassTitle}
+                className="sort"
+              >
+                <BsArrowUpDown />
+              </button>
+            </th>
+            <th>
+              Last Class Date
+              <button
+                type="button"
+                onClick={sortByLastClassDate}
+                className="sort"
+              >
+                <BsArrowUpDown />
+              </button>
+            </th>
             <th>Last Class Time</th>
-            <th>Avg Class Rating</th>
+            <th>
+              Avg Class Rating
+              <button type="button" onClick={sortByRating} className="sort">
+                <BsArrowUpDown />
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {students.map((item) => {
-            let getStars = 0;
-            let totalStarts = item.classes_registered.length * 5;
-            item.classes_registered.map((stars) => {
-              getStars = getStars + Number(stars.feedback.feedback);
-            });
-            return (
-              <tr key={item.id}>
-                <td data-column="Name">
-                  <Link
-                    className="t-data"
-                    to={{
-                      pathname: "/student",
-                      state: {
-                        pass: item.classes_registered,
-                        passName: item.name,
-                      },
-                    }}
-                  >
-                    {item.name}
-                  </Link>
-                </td>
-                <td data-column="Enrolled On">{item.created_at}</td>
-                <td data-column="Total classes ">
-                  {" "}
-                  {item.classes_registered.length}
-                </td>
+          {loading
+            ? ascendingAlphabetically
+              ? ascendingByName
+                ? ascendingByStudentName
+                  ? studentList
+                      .slice(0)
+                      .sort(function (a, b) {
+                        var nameA = a.name.toLowerCase();
+                        var nameB = b.name.toLowerCase();
+                        return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                      })
+                      .map((item) => {
+                        return studentData(item);
+                      })
+                  : studentList
+                      .slice(0)
+                      .sort(function (a, b) {
+                        var nameA = a.name.toLowerCase();
+                        var nameB = b.name.toLowerCase();
+                        return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                      })
+                      .reverse()
+                      .map((item) => {
+                        return studentData(item);
+                      })
+                : ascendingByLastClassTitle
+                ? studentList
+                    .slice(0)
+                    .sort(function (a, b) {
+                      var titleA = a.lastClassTitle.toLowerCase();
+                      var titleB = b.lastClassTitle.toLowerCase();
+                      return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+                    })
+                    .map((item) => {
+                      return studentData(item);
+                    })
+                : studentList
+                    .slice(0)
+                    .sort(function (a, b) {
+                      var titleA = a.lastClassTitle.toLowerCase();
+                      var titleB = b.lastClassTitle.toLowerCase();
+                      return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+                    })
+                    .reverse()
+                    .map((item) => {
+                      return studentData(item);
+                    })
+              : ascendingByDate
+              ? ascendingByEnrollDate
+                ? studentList.map((item) => {
+                    console.log("item", item);
+                    return studentData(item);
+                  })
+                : studentList
+                    .slice(0)
+                    .sort(function (a, b) {
+                      const dateA = new Date(a.created_at);
+                      const dateB = new Date(b.created_at);
+                      return dateB - dateA;
+                    })
+                    .reverse()
+                    .map((item) => {
+                      return studentData(item);
+                    })
+              : ascendingByLastDate
+              ? studentClassLastDate.map((item) => {
+                  return studentData(item);
+                })
+              : studentClassLastDate
+                  .slice(0)
+                  .sort(function (a, b) {
+                    const dateA = new Date(a.lastDate);
+                    const dateB = new Date(b.lastDate);
+                    return dateB - dateA;
+                  })
+                  .reverse()
+                  .map((item) => {
+                    return studentData(item);
+                  })
+            : ascendingNumerically
+            ? ascendingByTotalClass
+              ? studentList
+                  .slice(0)
+                  .sort(function (a, b) {
+                    var numberA = a.classes_registered.length;
+                    var numberB = b.classes_registered.length;
+                    return numberB - numberA;
+                  })
+                  .map((item) => {
+                    return studentData(item);
+                  })
+              : studentList
+                  .slice(0)
+                  .sort(function (a, b) {
+                    var numberA = a.classes_registered.length;
+                    var numberB = b.classes_registered.length;
+                    return numberA - numberB;
+                  })
+                  .map((item) => {
+                    return studentData(item);
+                  })
+            : ascendingByRating
+            ? studentList
+                .sort(function (a, b) {
+                  return b.avgClassRating - a.avgClassRating;
+                })
+                .map((item) => {
+                  return studentData(item);
+                })
+            : studentList
+                .sort(function (a, b) {
+                  return a.avgClassRating - b.avgClassRating;
+                })
+                .map((item) => {
+                  return studentData(item);
+                })}
 
-                <td data-column="Last class title">
-                  {item.classes_registered &&
-                  item.classes_registered.length > 0 &&
-                  item.classes_registered[item.classes_registered.length - 1][
-                    "title"
-                  ] != ""
-                    ? item.classes_registered[
-                        item.classes_registered.length - 1
-                      ]["title"]
-                    : "NA"}
-                </td>
-                <td data-column="Last class date">
-                  {item.classes_registered &&
-                  item.classes_registered.length > 0 &&
-                  item.classes_registered[item.classes_registered.length - 1][
-                    "start_time"
-                  ]
-                    ? item.classes_registered[
-                        item.classes_registered.length - 1
-                      ]["start_time"]
-                    : "NA"}
-                </td>
-                <td data-column="Last class time">
-                  {item.classes_registered &&
-                  item.classes_registered.length > 0 &&
-                  item.classes_registered[item.classes_registered.length - 1][
-                    "end_time"
-                  ]
-                    ? item.classes_registered[
-                        item.classes_registered.length - 1
-                      ]["end_time"]
-                    : "NA"}
-                </td>
-                <td data-column="Avg rating ">
-                  {[1, 2, 3, 4, 5].map((star) => {
-                    return Math.ceil(getStars / totalStarts) > 0 &&
-                      star <= Math.ceil(getStars / totalStarts) ? (
-                      <span
-                        className="fa fa-star"
-                        style={{ color: "#D55F31" }}
-                      ></span>
-                    ) : (
-                      <span
-                        className="fa fa-star"
-                        style={{ color: "gray" }}
-                      ></span>
-                    );
-                  })}
-                </td>
-              </tr>
-            );
-          })}
           {message ? <h1 className="Message">{message}</h1> : null}
         </tbody>
       </table>
